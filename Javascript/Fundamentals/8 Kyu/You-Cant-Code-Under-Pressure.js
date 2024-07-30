5d86c82cac2ec2163131b503b160e5e13e3d63e3
username <Jzazzi56@gmail.com>
2024-07-29 22:24:36 -0700

    you cant code under pressure

---
+++ Javascript/Fundamentals/8 Kyu/You-Cant-Code-Under-Pressure.js
@@ -0,0 +1,14 @@
+/*
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  const doubled = i * 2;
+  return doubled;
+}
+
+/*
+    approach:
+        - multiply the input by 2 and return the result
+*/

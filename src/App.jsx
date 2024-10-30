b032ddde8d579e4bed73bc2fd00728e90efafa7d
username <Jzazzi56@gmail.com>
2024-10-29 18:48:04 -0700

    added route to user form

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home/Home";
 import MagicEightball from "./pages/Mini-Projects/MagicEightball/MagicEightball";
 import TodoList from "./pages/Mini-Projects/Todo-List/Todo";
 import Counter from "./pages/Mini-Projects/Counter/Counter";
+import UserForm from "./pages/Mini-Projects/UserForm/UserForm";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/eightball" element={<MagicEightball />} />
           <Route path="/todo-list" element={<TodoList />} />
           <Route path="/counter" element={<Counter />} />
+          <Route path="/user-form" element={<UserForm />} />
         </Routes>
       </Router>
     </div>

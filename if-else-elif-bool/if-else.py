3170e0a53cbd232941ef5f0d75fc637ffea9d1fa
username <Jzazzi56@gmail.com>
2025-01-27 14:38:46 -0800

    removing notes from if-else.py

--- if-else-elif-bool/if-else.py
+++ if-else-elif-bool/if-else.py
@@ -1,24 +1,3 @@
-# Single line comment
-
-'''
-Multi-line comment
-'''
-
-'''
-Boolean expressions are statements that can be either True or False
-A boolean variable is a variable that is set to either True or False.
-We can create boolean expressions using relational operators:
-==: Equals
-!=: Not equals
->: Greater than
->=: Greater than or equal to
-<: Less than
-<=: Less than or equal to
-if statements can be used to create control flow in your code.
-else statements can be used to execute code when the conditions of an if statement are not met.
-elif statements can be used to build additional checks into your if statements
-'''
-
 # Example 1
 magic = True
 elf = True

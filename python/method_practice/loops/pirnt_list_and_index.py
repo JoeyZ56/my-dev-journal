edc555e21daa1f43034f69bca7d1e89e7806cdf9
username <Jzazzi56@gmail.com>
2025-08-04 12:08:29 -0700

    for loop getting index and value with enumerate

---
+++ python/method_practice/loops/pirnt_list_and_index.py
@@ -0,0 +1,6 @@
+#Print the index & value for each item in a list (enumerate).
+
+names = ["Alice", "Jasmine", "Symone", "Kiana", "Janessa"]
+
+for index, name in enumerate(names):
+    print(index, name)
